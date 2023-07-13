7f1920dcfa6f7c00b458daa6a652e51832e76a57
Darshan Ramoliya <mr.darshan2001@gmail.com>
2023-07-13 12:30:03 +0530

    changes.

--- insta/insta_app/views.py
+++ insta/insta_app/views.py
@@ -7,6 +7,8 @@ def hello(request):
     return HttpResponse('Hello, Welcome to my Instagram Clone.')
 
 def detail(request, id):
+    
     user_obj = Posts.objects.get(id = id)
     print(user_obj.user_name, user_obj.caption)
+
     return HttpResponse(user_obj)
